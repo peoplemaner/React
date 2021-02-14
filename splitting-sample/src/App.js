2e4d739b58d69db84fe5b648fcde96cad6a1781a
peoplemaner <peoplemaner@gmail.com>
2021-02-14 22:51:03 +0900

    code-splitting

---
+++ splitting-sample/src/App.js
@@ -0,0 +1,47 @@
+//import React, { useState, Suspense } from 'react';
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import loadable from '@loadable/component';
+//const SplitMe = React.lazy(() => import('./SplitMe'));
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback: <div>loading...</div>
+});
+/*
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>loading...</div>} >
+          {visible && <SplitMe />}
+        </Suspense>
+      </header>
+    </div>
+  );
+}*/
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React!</p>
+        {visible && <SplitMe />}
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- splitting-sample/src/index.js
+++ splitting-sample/src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import rootReducer from './modules/index';
-//import loggerMiddleware from './lib/loggerMiddleware';
-import { createLogger } from 'redux-logger';
-import ReduxThunk from 'redux-thunk';
-
-const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
